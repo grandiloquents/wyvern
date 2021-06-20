a32380ee0b7e92ca50426358e757697cdd1c1605
faiyaz <65500334+grandiloquents@users.noreply.github.com>
2021-06-20 14:22:42 +0600

    Update addmanager.js

--- commands/addmanager.js
+++ commands/addmanager.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-const client = require("vassal/main.js")
+const client = require("../main.js")
 module.exports = {
 	name: 'addmanager',
 	description: 'add pm/am role to someone',
