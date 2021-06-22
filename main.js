2e2508f1ba069cee275c9c5bbacb044b19cffcff
faiyaz <65500334+grandiloquents@users.noreply.github.com>
2021-06-22 10:20:37 +0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const config = require('./config.json');
 const prefix = config.prefix;
 const ownerId = config.ownerId;
-
+module.exports = client;
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
