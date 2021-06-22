e0811cd63ee1cd1fdbd9ccd330add1debe12c85b
faiyaz <65500334+grandiloquents@users.noreply.github.com>
2021-06-22 15:08:58 +0600

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -4,11 +4,15 @@ module.exports = {
 	name: 'say',
 	description: 'say smth in channel',
 	execute(message, args) {
+	const helpEmbed = new Discord.MessageEmbed()
+            .setDescription("**Usage:**\n```\n??say <channel/id> <message>\n```")
+            .setColor("#202225");
+	if (!args[0]) return message.channel.send(helpEmbed);
         let argsSplit = args.slice();
-		argsSplit.splice(0,1);
+	argsSplit.splice(0,1);
 
-		const sayMsg = argsSplit.join(" ");
-		const sayChannel = client.channels.cache.get(args[0].substring(2).substring(0,18)) || client.channels.cache.get(`${args[0]}`)
+	const sayMsg = argsSplit.join(" ");
+	const sayChannel = client.channels.cache.get(args[0].substring(2).substring(0,18)) || client.channels.cache.get(`${args[0]}`)
         const errEmbed = new Discord.MessageEmbed()
             .setDescription("An error occured while executing this command. Please check if you provided a valid channel.")
             .setColor("#202225");
